111a39185140250411eb59a8718e65b2beaa5ec5
Anurag <anuragmisraphy99@gmail.com>
2021-06-13 20:09:11 +0530

    chat-app

---
+++ socket.js
@@ -0,0 +1,55 @@
+function emo(mes) {
+    var rep = {
+        ':)' : '<img src="/similes/smile.gif" alt=":)"/>',
+        'B|' : '<img src="/similes/cool.gif" alt="B|"/>',
+        ':D' : '<img src="/similes/big_smile.gif" alt=":D"/>',
+        ':\'(' : '<img src="/similes/crying.gif" alt=":\'("/>',
+        'lol' : '<img src="/similes/lol.gif" alt="lol"/>',
+        'LOL' : '<img src="/similes/lol2.gif" alt="B|"/>',
+        ':|': '<img src="/similes/neutral.gif" alt=":|"/>',
+        ':(' : '<img src="/similes/sad.gif" alt=":("/>',
+        ':v' : '<img src="/similes/kidding.gif" alt=":v"/>',
+        ':p' : '<img src="/similes/tongue.gif" alt=":p"/>',
+        ':o' : '<img src="/similes/surprised.gif" alt=":o"/>',
+        ';)' : '<img src="/similes/wink.gif" alt=";)"/>',
+        ':@' : '<img src="/similes/angry.gif" alt=":@"/>'
+    };
+    for (var key in rep) {
+        mes = mes.replace(key, rep[key]);
+    }
+    return mes;
+}
+var online = [];
+
+module.exports = function (server) {
+
+    var io = require('socket.io').listen(server);
+    io.sockets.on('connection', function (socket) {
+
+        socket.on('new message', function (message) {
+            var nm = emo(message.body);
+            io.sockets.emit('message', {
+                main : message.from + " : " + nm,
+                from : message.from
+            });
+        });
+        socket.on('join', function (user) {
+            var mes = 'Welcome ' + user.name + ' ! <br />';
+            var ou = 'Online Users : ';
+            online.push(user.name);
+            online.forEach(function(d){
+                ou += d + ", ";
+            })
+            io.sockets.emit('joined', mes + ou);
+        });
+        socket.on('leave', function (user) {
+            online.splice(online.indexOf(user), 1);
+            io.sockets.emit('left',  user +' left..');
+        });
+        socket.on('disconnect', function () {
+           console.log('Disconnected Client..');
+
+        });
+    });
+
+};
---
+++ views/assets/js/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    var msg = $('.status');
+    var box = $('#box');
+    var sound = $('#alert')[0];
+    var socket = io.connect('/');
+    var username = prompt('Your name :');
+    if (username != '') {
+        socket.emit('join', { name: username})
+    }
+    function cmsg() {
+        msg.css('color', 'green');
+        msg.html('Connected !');
+        msg.fadeOut(500);
+    }
+
+    function showbox(message) {
+        box.append('<br />' + message + '<br />');
+        box.animate({ scrollTop: box.prop("scrollHeight") - box.height() + 100 }, 250);
+    }
+
+    socket.on('joined', function (join) {
+        showbox(join);
+    });
+    socket.on('left', function (message) {
+        showbox(message)
+    });
+
+    socket.on('message', function (msg) {
+        if (msg.from != username) {
+            sound.play();
+        }
+        showbox(msg.main);
+    });
+
+    function sendmessage(e) {
+        e.preventDefault();
+        var text = $('.msg').val();
+        if (text == '') {
+            return false;
+        }
+        $('.msg').val('');
+        var message = {
+            body: text,
+            from: username,
+            by: 'user'
+        };
+        socket.emit('new message', message);
+    }
+
+    $(document).keypress(function (e) {
+        if (e.which == 13) {
+            sendmessage(e);
+        }
+    });
+    $('.send').click(function (e) {
+        sendmessage(e)
+
+    });
+
+    $(window).unload(function () {
+        socket.emit('leave', username);
+    });
+
+    socket.on('disconnect', function () {
+        msg.css('color', 'red');
+        msg.html('Disconnected !');
+        msg.fadeIn(500);
+    });
+});
